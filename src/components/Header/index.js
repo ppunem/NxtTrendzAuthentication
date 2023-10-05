2f037ce9258ff206bb9afa099220ffea3ea96e6e
ppunem <128507331+ppunem@users.noreply.github.com>
2023-10-05 14:27:43 +0530

    Update index.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,8 +2,7 @@
 import {Link} from 'react-router-dom'
 import './index.css'
 
-const Header = () => {
-    return (
+const Header = () =>(
           <nav className="header-container">
             <img className="logo" src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png" alt="website logo"/>
             <div className="nav-options-container">
@@ -14,6 +13,5 @@ const Header = () => {
             </div>
           </nav>
     )
-}
 
 export default Header
