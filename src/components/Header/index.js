13b0422634a6f0bb9ddb3ef9cb012be35c02d499
ppunem <128507331+ppunem@users.noreply.github.com>
2023-10-07 19:57:44 +0530

    Update index.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,12 +5,12 @@ import './index.css'
 const Header = () =>(
           <nav className="header-container">
             <img className="logo" src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png" alt="website logo"/>
-            <div className="nav-options-container">
-              <Link to="/"><p className="nav-item">Home</p></Link>
-              <Link to="/product"><p className="nav-item">Products</p></Link>
-              <Link to="/cart"><p className="nav-item">Cart</p></Link>
-              <button className="logout-button" type="button">Logout</button>
-            </div>
+            <ul className="nav-options-container">
+              <li className="nav-item">Home</li>
+              <li className="nav-item">Products</li>
+              <li className="nav-item">Cart</li>
+              <li><button className="logout-button" type="button">Logout</button></li>
+            </ul>
           </nav>
     )
 
